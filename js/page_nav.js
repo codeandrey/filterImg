745f7a0799eb9d50b8efd1fd5f8702b917a5439f
codeandrey@ukr.net <codeandrey@ukr.net>
2018-07-21 23:16:14 +0300

    secundary commit

--- js/page_nav.js
+++ js/page_nav.js
@@ -30,7 +30,7 @@ var Pagination = {
     Click: function() {
         Pagination.page = +this.innerHTML;
 
-        renderdisplay(LoadImg(Pagination.page));
+        FilterImg.renderdisplay(FilterImg.LoadImg(Pagination.page));
 
         Pagination.Start();
     },
@@ -42,7 +42,7 @@ var Pagination = {
             Pagination.page = 1;
         }
 
-        renderdisplay(LoadImg(Pagination.page));
+        FilterImg.renderdisplay(FilterImg.LoadImg(Pagination.page));
 
         Pagination.Start();
     },
@@ -54,7 +54,7 @@ var Pagination = {
             Pagination.page = Pagination.size;
         }
        
-        renderdisplay(LoadImg(Pagination.page));
+        FilterImg.renderdisplay(FilterImg.LoadImg(Pagination.page));
 
         Pagination.Start();
     },
@@ -140,6 +140,249 @@ var Pagination = {
 
 
 
+var FilterImg = {
+    promiseImages:[],
+    mylist:[],
+
+    myCollection : function (){
+        var myList =FilterImg.promiseImages.then(function(images) {
+            var list=[];
+            var arr = JSON.parse(localStorage.getItem("myImg"))||[];
+            for (var i=0; i<arr.length; i++)   {
+                for (var n=0; n<images.length; n++)   {
+                    if (arr[i]==images[n].id){
+                       list[i]=images[n];
+                    }
+                }
+            }
+            return list;
+        });
+        return myList;
+    },
+ 
+    imgStore : function(imgId){
+        var arr = JSON.parse(localStorage.getItem("myImg"))||[];
+        for (var i = 0; i<arr.length; i++) {
+            if (arr[i]==imgId) {
+               return true;
+           }else{
+            return false;
+           }
+        }
+    },
+
+    filterSise: function(arr, size){
+        var res =arr.then(function(images) {
+            var filterResult=[];
+            for (var i = 0; i<images.length; i++){
+                if ((images[i].width>size['minWidth'])&&(images[i].width<size['maxWidth'])) {
+                    filterResult[i]=images[i];
+                }
+            }
+            return  filterResult;
+          });
+        return res;
+    },
+
+    filterAuthor: function(users, author){
+        var res = users.then(function(arr) {
+            var filterResult=[];
+            for (var i = 0; i<arr.length; i++){
+                if (arr[i].user['id'] == author) {
+                  filterResult[i]=arr[i];
+                }
+            }
+               return  filterResult;
+        });
+        return res;
+    },
+    
+    SizeImgChecked: function (){
+        var el = document.getElementById('radio__group'),
+            radios=el.querySelectorAll('input'),
+            filterImg=[],
+            check;
+
+        for (var i = 0; i < radios.length; i++) {
+            if (radios[i].type === 'radio' && radios[i].checked) {
+                check = radios[i].value;    
+            }
+        }
+        if (check=='all'){
+          filterImg['minWidth'] = 0; filterImg['maxWidth'] =10000;
+        }   
+          else if (check=='large'){
+            filterImg['minWidth']= 4500; filterImg['maxWidth'] =10000;
+          }
+            else if (check=='medium'){
+              filterImg['minWidth']= 2500; filterImg['maxWidth'] =4499;
+            }   
+              else if (check=='small') {
+                filterImg['minWidth']= 0; filterImg['maxWidth'] =2499;
+              }
+
+        return filterImg;
+    },
+
+    AuthorChecked: function (){
+        var el = document.getElementById('author__group'),
+            radios=el.querySelectorAll('input'),
+            check;
+
+        for (var i = 0; i < radios.length; i++) {
+            if (radios[i].type === 'radio' && radios[i].checked) {
+              check = radios[i].id;    
+            }
+        }
+        return check;
+    },
+
+    LoadImg: function(page){
+       
+        getJSON = function(url) {
+            return new Promise(function(resolve, reject) {
+                var xhr = new XMLHttpRequest();
+
+                xhr.open('get', url, true);
+                xhr.responseType = 'json';
+                xhr.onload = function() {
+                    var status = xhr.status;
+                    if (status == 200) {
+                        arr=xhr.response;
+                        Pagination.size= parseInt(xhr.getResponseHeader('x-total'));
+                        resolve(arr);
+                    } else {
+                        reject(status);
+                    }
+                };
+                xhr.send();
+            });
+        };
+    
+        query_url='https://api.unsplash.com/photos/?client_id=e879405519ab1a1043728365faeaa75dda8ff109fc47df38aef56b7a647860fe&page='+page+'&per_page=200';
+        var  dataResult =getJSON(query_url).then(function(response) {
+            return response;           
+            }, function(status) { 
+              
+        });
+        return dataResult;
+    },
+
+    LoadAuthor: function (){
+        FilterImg.promiseImages.then(function(author) {
+           
+            for(var i = 0; i < arr.length; i++){
+                var count = 1;
+                for (var j = 1; j < arr.length-1; j++) {
+                    if (arr[i].user['id']== arr[j].user['id']){
+                       count++;
+                   }
+                }
+
+
+                //     }
+                //     console.log(author);
+                // }
+    
+            
+            
+                var author = document.createElement('input');
+                var label =document.createElement('label'); 
+                var span =document.createElement('span'); 
+
+                author.type= 'radio';
+                author.name = 'author';
+                author.id= arr[i].user['id'];
+                author.value=arr[i].user['name'];
+
+                label.appendChild(author);
+                label.append(arr[i].user['name']);
+                span.innerHTML=' ['+count+']';
+                label.append(span);
+                document.getElementById('author__group').appendChild(label)
+            }
+            
+        });
+    },
+    
+    Bind: function(){
+        var radioClick = document.getElementById('radio__group');
+            radioClick.addEventListener('click',  function(e){ 
+                FilterImg.renderdisplay(FilterImg.filterSise(FilterImg.promiseImages, FilterImg.SizeImgChecked()));
+            }, false); 
+
+        var authorClick = document.getElementById('author__group');
+            authorClick.addEventListener('click',  function(e){ 
+              if  (FilterImg.AuthorChecked()=='all-author' ){
+                    FilterImg.renderdisplay(FilterImg.promiseImages);
+                }else {
+                    FilterImg.renderdisplay(FilterImg.filterAuthor(FilterImg.promiseImages, FilterImg.AuthorChecked()));
+                }
+            }, false); 
+        var gridClick = document.getElementById('grid');
+            gridClick.addEventListener('click', function(e){
+            
+                var click_id = event.srcElement.id;
+           
+                FilterImg.mylist.push(click_id);
+
+                var serialObj = JSON.stringify(mylist); 
+
+                localStorage.setItem("myImg", serialObj); 
+
+            }, false);
+
+        var mylistClick = document.getElementById('mylist');
+            mylistClick.addEventListener('click', function(e){
+            FilterImg.promiseImages=FilterImg.myCollection();
+                FilterImg.renderdisplay(FilterImg.filterSise(FilterImg.promiseImages, FilterImg.SizeImgChecked())); 
+        });
+    },
+    imgStore : function(imgId){
+            var arr = JSON.parse(localStorage.getItem("myImg"))||[];
+            for (var i = 0; i<arr.length; i++) {
+                if (arr[i]==imgId) {
+                   return true;
+               }else{
+                return false;
+               }
+            }
+            
+    },
+    renderdisplay: function(arr){
+        arr.then(function(pic) {
+
+            var item = document.getElementById("grid");
+            item.innerHTML = '';
+            if (typeof pic !== 'undefined' && pic.length > 0){
+                for(var i=0; i<pic.length; i++){
+                    var div = document.createElement('div');
+
+                    var span = document.createElement('span');
+
+                    var img = document.createElement('img');
+                    img.src=pic[i].urls['small'];
+                    img.id=pic[i].id;
+                    div.appendChild(img);
+                   if(FilterImg.imgStore(pic[i].id)){ 
+                        span.innerHTML='&#x2605';
+                    }else{
+                        span.innerHTML='&#x2606;';
+                    }
+                    div.append(span);
+                    document.getElementById('grid').appendChild(div);
+                }
+            }
+        });
+    },
+    Init: function(e, data) {
+        FilterImg.promiseImages = FilterImg.LoadImg( 1 );
+        FilterImg.renderdisplay( FilterImg.promiseImages ); 
+        FilterImg.LoadAuthor();
+        FilterImg.Bind();
+    }
+}
+
 /* * * * * * * * * * * * * * * * *
 * Initialization
 * * * * * * * * * * * * * * * * */
@@ -151,251 +394,10 @@ var init = function() {
       //  step: 2,   // pages before and after current
         page_per: 20
     });
+    FilterImg.Init();
+
 };
 
 document.addEventListener('DOMContentLoaded', init, false);
 
-var promiseImages;
-
-
-var myListImg = function (){
-   
-  
-     var ts =promiseImages.then(function(images) {
-        var   list=[];
-        var arr = JSON.parse(localStorage.getItem("myImg"))||[];
-        for (var i=0; i<arr.length; i++)   {
-                 for (var n=0; n<images.length; n++)   {
-   
-          if (arr[i]==images[n].id){
-            list[i]=images[n];
-      }
-         
-       }
-   }
-        return list;
-     });
-    return ts;
- }
- 
-var imgStore = function(imgId){
-    var arr = JSON.parse(localStorage.getItem("myImg"))||[];
-    for (var i = 0; i<arr.length; i++) {
-        if (arr[i]==imgId) {
-           return true;
-       }else{
-        return false;
-       }
-    }
-    
-
-
-
-}
-
-var filterSise = function(arr, size){
-
-  var res =arr.then(function(images) {
-    var filterResult=[];
-    for (var i = 0; i<images.length; i++){
-       if ((images[i].width>size['minWidth'])&&(images[i].width<size['maxWidth'])) {
-          filterResult[i]=images[i];
-       }
-    }
-    return  filterResult;
-  });
-  return res;
-} 
-
-var filterAuthor = function(users, author){
-  
-  var res = users.then(function(arr) {
-    var filterResult=[];
-      for (var i = 0; i<arr.length; i++){
-
-        if (arr[i].user['id'] == author) {
-          filterResult[i]=arr[i];
-        }
-      }
-       return  filterResult;
-   });
-  
-   return res;
-}
-
-var SizeImgChecked = function (){
-    var el = document.getElementById('radio__group'),
-        radios=el.querySelectorAll('input'),
-        filterImg=[],
-        check;
-
-    for (var i = 0; i < radios.length; i++) {
-        if (radios[i].type === 'radio' && radios[i].checked) {
-            check = radios[i].value;    
-        }
-    }
-    if (check=='all'){
-      filterImg['minWidth'] = 0; filterImg['maxWidth'] =10000;
-    }   
-      else if (check=='large'){
-        filterImg['minWidth']= 4500; filterImg['maxWidth'] =10000;
-      }
-        else if (check=='medium'){
-          filterImg['minWidth']= 2500; filterImg['maxWidth'] =4499;
-        }   
-          else if (check=='small') {
-            filterImg['minWidth']= 0; filterImg['maxWidth'] =2499;
-          }
-
-    return filterImg;
-}
-
-var AuthorChecked = function (){
-  var el = document.getElementById('author__group'),
-      radios=el.querySelectorAll('input'),
-      check;
-
-  for (var i = 0; i < radios.length; i++) {
-    if (radios[i].type === 'radio' && radios[i].checked) {
-      check = radios[i].id;    
-    }
-  }
-  return check;
-}
-
-var LoadImg = function(page){
-   
-    getJSON = function(url) {
-        return new Promise(function(resolve, reject) {
-            var xhr = new XMLHttpRequest();
-
-            xhr.open('get', url, true);
-            xhr.responseType = 'json';
-            xhr.onload = function() {
-                var status = xhr.status;
-                if (status == 200) {
-                    arr=xhr.response;
-                    Pagination.size= parseInt(xhr.getResponseHeader('x-total'));
-                    resolve(arr);
-                } else {
-                    reject(status);
-                }
-            };
-            xhr.send();
-        });
-    };
-    
-    query_url='https://api.unsplash.com/photos/?client_id=e879405519ab1a1043728365faeaa75dda8ff109fc47df38aef56b7a647860fe&page='+page+'&per_page=200';
-    var  dataResult =getJSON(query_url).then(function(response) {
-        return response;           
-        }, function(status) { 
-          
-    });
-    return dataResult;
-}
-
-var LoadAuthor=function (){
-    promiseImages.then(function(arr) {
-        console.log(arr);
-    for(var i=0; i<arr.length; i++){
-        var author = document.createElement('input');
-        var label =document.createElement('label'); 
-        var span =document.createElement('span'); 
-        author.type= 'radio';
-        author.name = 'author';
-        author.id= arr[i].user['id'];
-        author.value=arr[i].user['name'];
-        label.appendChild(author);
-        label.append(arr[i].user['name']);
-        label.append(span);
-        document.getElementById('author__group').appendChild(label)
-    }
-    });
-
-    
-}
-    
-var radioClick = document.getElementById('radio__group');
-    radioClick.addEventListener('click',  function(e){ 
-    renderdisplay(filterSise(promiseImages, SizeImgChecked()));
-    }, false); 
-
-var authorClick = document.getElementById('author__group');
-    authorClick.addEventListener('click',  function(e){ 
-      if  (AuthorChecked()=='all-author' ){
-            renderdisplay(promiseImages);
-        }else {
-            renderdisplay(filterAuthor(promiseImages,AuthorChecked()));
-        }
-    }, false); 
-
-
-
-var mylist=[];
-
-var gridClick = document.getElementById('grid');
-    gridClick.addEventListener('click', function(e){
-    
-        var click_id = event.srcElement.id;
-        // event.srcElement.nextNode().innerHTML='&#x2605';
-   
-        mylist.push(click_id);
-
-        var serialObj = JSON.stringify(mylist); 
-
-        localStorage.setItem("myImg", serialObj); 
-
-    }, false);
-
-var imgStore = function(imgId){
-    var arr = JSON.parse(localStorage.getItem("myImg"))||[];
-    for (var i = 0; i<arr.length; i++) {
-        if (arr[i]==imgId) {
-           return true;
-       }else{
-        return false;
-       }
-    }
-    
-}
-
-var mylistClick = document.getElementById('mylist');
-    mylistClick.addEventListener('click', function(e){
-     promiseImages=myListImg();
-        renderdisplay(filterSise(promiseImages, SizeImgChecked())); 
-    });
-
-var renderdisplay = function(arr){
-    arr.then(function(pic) {
-
-        var item = document.getElementById("grid");
-        item.innerHTML = '';
-        if (typeof pic !== 'undefined' && pic.length > 0){
-            for(var i=0; i<pic.length; i++){
-                var div = document.createElement('div');
-
-                var span = document.createElement('span');
-
-                var img = document.createElement('img');
-                img.src=pic[i].urls['small'];
-                img.id=pic[i].id;
-                div.appendChild(img);
-               if(imgStore(pic[i].id)){ 
-                    span.innerHTML='&#x2605';
-                }else{
-                    span.innerHTML='&#x2606;';
-                }
-                div.append(span);
-                document.getElementById('grid').appendChild(div);
-            }
-        }
-    });
-
-
-}
-
-promiseImages=LoadImg(1);
-
-renderdisplay(promiseImages); 
-LoadAuthor();
 
